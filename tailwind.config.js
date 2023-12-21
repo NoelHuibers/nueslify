562104bb98c79c679c76f8725ba34ff7b6825be2
Piibo <45877121+Piibo@users.noreply.github.com>
2023-12-21 11:45:48 +0100

    style interests

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
   theme: {
     extend: {
       brightness: {
