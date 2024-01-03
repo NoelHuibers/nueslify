0f1a9cb2e7003995aaaf682264cd04713b541a58
Piibo <45877121+Piibo@users.noreply.github.com>
2024-01-03 16:55:21 +0100

    update codemod

--- src/pages/interests/index copy.tsx
+++ src/pages/interests/index copy.tsx
@@ -1,6 +1,6 @@
 // FavoritesSelectionPage.tsx
 import React, { useState } from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Link from "next/link";
 
 interface Genre {
