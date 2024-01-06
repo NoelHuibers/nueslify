9fab10eaefe032ea3111d47a26fb07bf8c2c2651
Gabriel Knoll <gabriel.knoll@icloud.com>
2024-01-06 18:44:25 +0100

    push current state

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,10 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { ttsRouter } from "./routers/tts";
-import {gptRouter} from "~/server/api/routers/gptRouter";
+import { gptRouter } from "~/server/api/routers/gptRouter";
 import { spotifyRouter } from "./routers/topTracks";
-import {playbackRouter} from "~/server/api/routers/playback";
+import { playbackRouter } from "~/server/api/routers/playback";
+import { mixerRouter } from "./routers/mixer";
 
 /**
  * This is the primary router for your server.
@@ -16,6 +17,7 @@ export const appRouter = createTRPCRouter({
   gpt: gptRouter,
   spotify: spotifyRouter,
   playback: playbackRouter,
+  mixer: mixerRouter,
 });
 
 // export type definition of API
