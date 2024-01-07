ab284a23107553773370946f52614663cc3dd236
NoelHuibers <noel@huibers.io>
2024-01-07 16:14:51 +0100

    Fix

--- src/server/api/routers/playback.ts
+++ src/server/api/routers/playback.ts
@@ -4,8 +4,7 @@ import { refreshSpotifyToken } from "~/utils/getTopTracks";
 import WebPlayback from "~/pages/playback";
 
 export const playbackRouter = createTRPCRouter({
-    playback: protectedProcedure.query(async ({ ctx }) => {
-
-        return await refreshSpotifyToken(ctx.session.user.id);
-    }),
+  playback: protectedProcedure.query(async ({ ctx }) => {
+    return await refreshSpotifyToken(ctx.session.user.id);
+  }),
 });
