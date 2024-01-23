3cf7e123cdc3ffa300c4513bfe3738c3ad342be1
NoelHuibers <noel@huibers.io>
2024-01-23 01:03:53 +0100

    Remove example Router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { ttsRouter } from "./routers/tts";
 import { gptRouter } from "~/server/api/routers/gptRouter";
@@ -12,10 +11,9 @@ import { mixerRouter } from "./routers/mixer";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
-  tts: ttsRouter,
-  gpt: gptRouter,
-  spotify: spotifyRouter,
+  tts: ttsRouter, // TTS
+  gpt: gptRouter, // GPT
+  spotify: spotifyRouter, // Toptracks
   playback: playbackRouter,
   mixer: mixerRouter,
 });
