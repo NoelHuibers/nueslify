cba93b8be5389ff7573774cd659b4ea8b1e115d2
Sebastian Will <basti.c.will@gmail.com>
2024-01-28 14:17:23 +0100

    added LLM switch with langchain

--- src/env.mjs
+++ src/env.mjs
@@ -29,6 +29,7 @@ export const env = createEnv({
     AWS_ACCESS_KEY_ID: z.string(),
     AWS_SECRET_ACCESS_KEY: z.string(),
     AWS_REGION: z.string(),
+    GOOGLE_API_KEY: z.string(),
   },
 
   /**
@@ -55,6 +56,7 @@ export const env = createEnv({
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     AWS_REGION: process.env.AWS_REGION,
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
---
+++ src/utils/GPT/langchain.ts
@@ -0,0 +1,39 @@
+import {ChatOpenAI} from "@langchain/openai";
+import {ChatPromptTemplate} from "@langchain/core/prompts";
+import {StringOutputParser} from "@langchain/core/output_parsers";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import {env} from "~/env.mjs";
+
+type model = "openAI" | "gemini";
+
+const prompt = ChatPromptTemplate.fromMessages([
+    ["system", "You are Nueslify, a radio host"],
+    ["human", "{input}"],
+]);
+
+const runLLM = async (content: string, model: model) => {
+    let chatModel: any;
+
+    if (model == "openAI") {
+        chatModel = new ChatOpenAI({
+            openAIApiKey: env.OPENAI_API_KEY,
+        });
+    } else if (model == "gemini") {
+        chatModel = new ChatGoogleGenerativeAI({
+            apiKey: env.GOOGLE_API_KEY,
+        });
+    }
+
+    const outputParser = new StringOutputParser();
+    const llmChain = prompt.pipe(chatModel).pipe(outputParser);
+
+    const res = await llmChain.invoke({
+        input: content,
+    });
+
+    console.log(res)
+
+    return res
+};
+
+export default runLLM;
