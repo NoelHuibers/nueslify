1c984abc5bd4b590dbc738dc03a0dd3073ca886f
Piibo <45877121+Piibo@users.noreply.github.com>
2024-01-29 20:42:01 +0100

    icons

--- src/pages/dashboard/navbar.tsx
+++ src/pages/dashboard/navbar.tsx
@@ -1,25 +1,29 @@
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { IoIosSettings } from "react-icons/io";
+import { IoLogOut } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <nav className="flex h-fit w-full flex-row items-center p-8">
-      <h1 className="text-xl text-slate-50">Nueslify</h1>
+    <nav className="flex h-fit w-full items-center justify-between p-8">
+      <h1 className="text-xl text-slate-50">Dashboard</h1>
 
-      <button
-        className="duration-30 mx-auto mr-1 rounded-xl bg-indigo-200 px-8 py-4 text-xl font-bold text-indigo-900 transition hover:bg-emerald-300"
-        onClick={async () => {
-          await signOut({ callbackUrl: "/" });
-        }}
-      >
-        <p className="text-xl">Logout</p>
-      </button>
-      <Link
-        href="/interests"
-        className="cursor-pointer rounded-xl bg-indigo-200 px-8 py-4 text-xl font-bold text-indigo-900 transition duration-300 hover:bg-emerald-300"
-      >
-        Interests
-      </Link>
+      <div className="flex">
+        <Link
+          href="/interests"
+          className="duration-30 mr-1 rounded-xl bg-indigo-200 px-2 py-2 text-xl font-bold text-indigo-900 transition hover:bg-emerald-300"
+        >
+          <IoIosSettings size={50} />
+        </Link>
+        <button
+          className="duration-30 rounded-xl bg-indigo-200 py-2 pl-3 pr-1 text-xl font-bold text-indigo-900 transition hover:bg-emerald-300"
+          onClick={async () => {
+            await signOut({ callbackUrl: "/" });
+          }}
+        >
+          <IoLogOut size={50} />
+        </button>
+      </div>
     </nav>
   );
 };
