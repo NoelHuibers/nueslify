43491d8e1118a15c32953bb766a30d3c6c71004f
Piibo <45877121+Piibo@users.noreply.github.com>
2024-02-01 19:11:58 +0100

    Add openSpotify function and update onClick event handler in SpotifyButton component

--- src/pages/dashboard/sportifyButton.tsx
+++ src/pages/dashboard/sportifyButton.tsx
@@ -1,13 +1,15 @@
 import { FaSpotify } from "react-icons/fa";
 
+const openSpotify = (): void => {
+  window.open("https://www.spotify.com", "_blank", "noopener,noreferrer");
+};
+
 const SpotifyButton = () => {
   return (
     <div className="mt-3 flex justify-center">
       <button
         className="duration-30 flex items-center justify-center rounded-xl bg-indigo-200 p-4 text-xl font-bold text-indigo-900 transition hover:bg-emerald-300"
-        //onClick={async () => {
-        //await ...({ callbackUrl: "/" });
-        //}}
+        onClick={openSpotify}
       >
         <div className="flex items-center space-x-2">
           <FaSpotify size={40} />
